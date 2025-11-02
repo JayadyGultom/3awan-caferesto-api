c026b6b458eaea22b5b6e458afffd800e8ea954e
JayadyGultom <jayadigultom@gmail.com>
2025-11-02 20:04:27 +0700

    mengganti port menjadi 8080

--- app.py
+++ app.py
@@ -13,4 +13,6 @@ for r in semua_routes:
     app.register_blueprint(r)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(debug=False, host="0.0.0.0", port=port)
