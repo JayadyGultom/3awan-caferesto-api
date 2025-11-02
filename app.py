f14155ce5d5c603ffb04db73424348027e19a074
JayadyGultom <jayadigultom@gmail.com>
2025-11-02 20:36:32 +0700

    perbaikan: gunakan PORT dinamis untuk railway

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ for r in semua_routes:
 # Tambah route root sederhana (biar Railway tahu aplikasi hidup)
 @app.route("/")
 def home():
-    return {"message": "API is running 🚀"}
+    return {"message": "API is running"}
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # <- Gunakan port dari Railway
